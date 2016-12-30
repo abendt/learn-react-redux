d2893680957a15f942f76d39dc503970ae69dac9
Alphonse Bendt <alphonse.bendt@akquinet.de>
2016-12-31 00:04:39 +0100

    layout

--- src/components/App.js
+++ src/components/App.js
@@ -4,23 +4,21 @@ import VisibleTodoList from "../containers/VisibleTodoList";
 import AddTodo from "../containers/AddTodo";
 import Notification from "../containers/Notification";
 import UndoRedo from "../containers/UndoRedo";
-import {Grid, PageHeader} from "react-bootstrap";
-
+import {Grid, Panel} from "react-bootstrap";
 
 const App = ({params}) => (
     <Grid>
-        <PageHeader>Todo App</PageHeader>
+        <Panel header={<h3>Todo App</h3>}>
+            <AddTodo />
 
-        <AddTodo />
+            <VisibleTodoList />
 
-        <VisibleTodoList />
+            <Footer />
 
-        <Footer />
+            <UndoRedo />
 
-        <UndoRedo />
-
-        <Notification/>
-
+            <Notification/>
+        </Panel>
     </Grid>
 );
 
