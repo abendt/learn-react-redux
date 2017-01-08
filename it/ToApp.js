d5654bfe5cc3d4135760bede6c2c1aec7fd24e04
Alphonse Bendt <alphonse.bendt@akquinet.de>
2017-01-08 18:48:04 +0100

    e2e test

---
+++ it/ToApp.js
@@ -0,0 +1,63 @@
+var EC = protractor.ExpectedConditions;
+
+describe('Todo App', function () {
+
+    var addTodoButton = element(by.id('addTodoButton'));
+    var addTodoText = element(by.id('addTodoText'));
+    var todoRows = element.all(by.id('todoItemRow'));
+    var todoItems = todoRows.all(by.id('item'));
+    var filterLinks = element.all(by.id('filterLink'));
+
+    it('can add Todo', function () {
+        browser.get('/');
+
+        addTodoText.sendKeys("new TODO");
+        addTodoButton.click();
+
+        browser.wait(presenceOfAll(todoRows), 1000);
+
+        filterLinks.get(2).click();
+
+        browser.wait(EC.invisibilityOf($("#todoItemRow #item")), 1000);
+
+        filterLinks.get(1).click();
+
+        const todo = todoItems.first();
+
+        expect(todo.getText()).toBe('new TODO');
+
+        todoItems.get(0).click();
+
+        browser.wait(EC.invisibilityOf($("#todoItemRow #item")), 1000);
+
+        filterLinks.get(0).click();
+
+        todoRows.all(by.id("deleteTodo")).click();
+
+        browser.wait(EC.invisibilityOf($("#todoItemRow #item")), 1000);
+
+    });
+
+});
+
+const presenceOfAll = (elementArrayFinder) => function () {
+    return elementArrayFinder.count((count) => {
+        return count > 0;
+    });
+};
+
+
+const highlightElement = (el) => {
+    console.log("highlight--");
+
+    console.log("locator---:" + el.locator());
+
+    return browser.driver.executeScript("arguments[0].setAttribute('style', arguments[1]);",
+        el.getWebElement(),
+        "color: Red; border: 2px solid red;").then(() => {
+        browser.sleep(2000);
+        return el;
+    }, function (err) {
+        console.log("error is :" + err);
+    });
+};
