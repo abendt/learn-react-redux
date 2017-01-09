3ac99393ff16080f2314b845e81d89fa8f64984b
Alphonse Bendt <alphonse.bendt@akquinet.de>
2017-01-09 22:08:42 +0100

    redux-logger verwenden

--- src/model/configureStore.js
+++ src/model/configureStore.js
@@ -3,31 +3,12 @@ import todoApp from "../reducers";
 import promise from "redux-promise";
 import createLogger from "redux-logger";
 
-const logging = (store) => (dispatch) => {
-
-    if (!console.group) {
-        return dispatch;
-    }
-
-    return (action) => {
-        console.group(action.type);
-
-        console.log("%c prev state", 'color: gray', store.getState());
-        console.log("%c action", 'color: blue', action);
-        const result = dispatch(action);
-        console.log("%c next state", 'color: green', store.getState());
-        console.groupEnd(action.type);
-
-        return result;
-    };
-};
-
 const configureStore = () => {
     const loadedState = undefined;
 
     const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-    const middlewares = [promise, logging];
+    const middlewares = [promise, createLogger()];
 
     // Store mit Support für Chrome Dev Tools
     const store = createStore(todoApp, loadedState, composeEnhancers(applyMiddleware(...middlewares)));
